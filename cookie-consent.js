d390065f9d5623c25fd3c9854c6d70bbcf7564dd
colinconbe <colin.conbe@gmail.com>
2026-06-20 09:59:45 +0000

    Ajout Microsoft Clarity (heatmaps et session recordings)

--- cookie-consent.js
+++ cookie-consent.js
@@ -15,3 +15,17 @@
   gtag("js", new Date());
   gtag("config", GA_MEASUREMENT_ID);
 })();
+
+// Chargement de Microsoft Clarity (heatmaps & session recordings)
+(function (c, l, a, r, i, t, y) {
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  t = l.createElement(r);
+  t.async = 1;
+  t.src = "https://www.clarity.ms/tag/" + i;
+  y = l.getElementsByTagName(r)[0];
+  y.parentNode.insertBefore(t, y);
+})(window, document, "clarity", "script", "x9xocgy2rx");
